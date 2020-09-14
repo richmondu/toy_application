f8557d36dd15aca3399e30502a7b51cb9dad0d27
richmondu <richmond.umagat@yahoo.com>
2020-09-14 15:28:46 +0800

    Update test_curl_upload_localhost.bat

--- curltest/test_curl_upload_localhost.bat
+++ curltest/test_curl_upload_localhost.bat
@@ -2,11 +2,11 @@ set UPLOAD_API="http://127.0.0.1:8000/api/v1/objectdetection/image"
 
 cd test_images\input\
 
-curl -H "Content-Type: multipart/form-data" -X POST %UPLOAD_API% -F "image=@image.jpg" 
-curl -H "Content-Type: multipart/form-data" -X POST %UPLOAD_API% -F "image=@image__blur4.0.jpg" 
-curl -H "Content-Type: multipart/form-data" -X POST %UPLOAD_API% -F "image=@image__fliph.jpg" 
-curl -H "Content-Type: multipart/form-data" -X POST %UPLOAD_API% -F "image=@image__rot180.jpg" 
-curl -H "Content-Type: multipart/form-data" -X POST %UPLOAD_API% -F "image=@image__zoom200_0_300_300.jpg"
+curl -X POST %UPLOAD_API% -F "image=@image.jpg" 
+curl -X POST %UPLOAD_API% -F "image=@image__blur4.0.jpg" 
+curl -X POST %UPLOAD_API% -F "image=@image__fliph.jpg" 
+curl -X POST %UPLOAD_API% -F "image=@image__rot180.jpg" 
+curl -X POST %UPLOAD_API% -F "image=@image__zoom200_0_300_300.jpg"
 
 pause
 
